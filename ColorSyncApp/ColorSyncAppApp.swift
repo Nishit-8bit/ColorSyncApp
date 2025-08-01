50b3534f4f5349ad7903e1bbcc22f7267b55e944
Nishit Singh <singhnishit786@gmail.com>
2025-08-01 06:32:32 +0530

    Initial Commit

---
+++ ColorSyncApp/ColorSyncAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSyncAppApp.swift
+//  ColorSyncApp
+//
+//  Created by Nishit Choudhary on 01/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorSyncAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
